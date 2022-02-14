c9c2920690fe5e76c2a049bcf9bed5fc391da5f0
Thiago <thiago.guareschi@ibiruba.ifrs.edu.br>
2022-02-14 17:18:07 -0300

    Add files via upload

---
+++ ifmaker_db.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 14-Fev-2022 às 21:17
+-- Versão do servidor: 10.4.20-MariaDB
+-- versão do PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `ifmaker_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `arquivo`
+--
+
+CREATE TABLE `arquivo` (
+  `codigo` int(11) NOT NULL,
+  `arquivo` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `data` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `arquivo`
+--
+
+INSERT INTO `arquivo` (`codigo`, `arquivo`, `data`) VALUES
+(1, 'a51a30d7734a484a8a1902b2e47fb9d2.jpg', '2022-01-25 18:30:10'),
+(2, '3deb56d4ab5f32405d6ace2719d60e12.zip', '2022-01-25 18:31:10');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `projeto`
+--
+
+CREATE TABLE `projeto` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `responsavel` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `equipamento` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `resumo` text COLLATE utf8_unicode_ci NOT NULL,
+  `data` timestamp NOT NULL DEFAULT current_timestamp(),
+  `anexo` varchar(40) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `projeto`
+--
+
+INSERT INTO `projeto` (`id`, `titulo`, `responsavel`, `equipamento`, `resumo`, `data`, `anexo`) VALUES
+(6, 'Projeto 1', 'Thiago', 'Impressora 3D', 'Sei lá\r\n', '2022-01-25 20:36:37', ''),
+(7, 'Projeto 2', 'Thiago', 'Impressorassora 4', 'f', '2022-01-25 21:00:11', ''),
+(8, 'TESTE', 'TESTE', 'testeab', 'frara', '2022-01-25 21:04:52', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `senha` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `data` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`, `data`) VALUES
+(2, 'Thiago Guareschi', 'thiago.guareschi@ibiruba.ifrs.edu.br', 's49p20t1', '2022-02-14 19:09:26');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `arquivo`
+--
+ALTER TABLE `arquivo`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Índices para tabela `projeto`
+--
+ALTER TABLE `projeto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `arquivo`
+--
+ALTER TABLE `arquivo`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `projeto`
+--
+ALTER TABLE `projeto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
